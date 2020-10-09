5495d402b601b6b3f218438dc65ee69f0c266cb3
Fonta22 <61181201+Fonta22@users.noreply.github.com>
2020-10-09 13:51:50 +0200

    Added Actualtime.java (now in Projects folder)
    That's the same program I've Pull Requested, but now on the correct folder.

---
+++ Java/Projects/Actualtime.java
@@ -0,0 +1,16 @@
+// Import what we need.
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+// Create class.
+public class Actualtime {
+    // Create main method.
+    public static void main(String[] args) {
+        // Set the date/time format we want.
+        DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        // Print the result.
+        System.out.println(timeformat.format(now));
+    }
+}
+// By Fonta22
