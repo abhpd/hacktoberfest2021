da78f2c479e780416dd23b385aa1ea9d8ae00cb8
Jennifer Rosa <rosajen27@gmail.com>
2020-10-09 17:10:02 -0400

    Odd or Even algorithm

---
+++ Javascript/Algorithms/odd-or-even.js
@@ -0,0 +1,11 @@
+// returns the string "odd" if `num` is an odd number
+// and returns the string "even" if `num` is an even number
+
+var oddOrEven = function(num) {
+    if (num % 2 === 0) {
+      return "even";
+    } else {
+      return "odd";
+    }
+  };
+  
