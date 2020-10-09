e62cf95cbb2452a786665d7fe4a5f96808149096
Jennifer Rosa <rosajen27@gmail.com>
2020-10-09 17:06:40 -0400

    Create arithmetic.js

---
+++ Javascript/Algorithms/arithmetic.js
@@ -0,0 +1,18 @@
+// add, subtract, multiple, or divide two numbers
+
+var add = function(num1, num2) {
+    return num1 + num2;
+  };
+  
+  var subtract = function(num1, num2) {
+    return num1 - num2;
+  };
+  
+  var multiply = function(num1, num2) {
+    return num1 * num2;
+  };
+  
+  var divide = function(num1, num2) {
+    return num1 / num2;
+  };
+  
