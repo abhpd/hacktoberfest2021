d4e4359f2e01d67aa358bfe2291669a260b60ef1
debasish-123 <72218056+debasish-123@users.noreply.github.com>
2020-10-10 20:12:30 +0530

    Plus Minus Problem
    In an array of integers, we're calculating the ratios of its elements that are positive, negative, and zero. Also, printing the decimal value of each fraction on a new line with 6  places after the decimal.

---
+++ Java/Projects/Plus Minus.java
@@ -0,0 +1,61 @@
+package plusMinus;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Plus Minus {
+
+   
+    static void plusMinus(int[] arr) { 
+        int n= arr.length; 
+  
+        float positiveCount = 0; 
+        float negativeCount = 0; 
+        float zeroCount = 0; 
+ 
+        for (int i = 0; i < n; i++) { 
+            if (arr[i] > 0) { 
+                positiveCount++; 
+            } 
+            else if (arr[i] < 0) { 
+                negativeCount++; 
+            } 
+            else if (arr[i] == 0) { 
+                zeroCount++; 
+            } 
+        } 
+   
+        System.out.printf("%1.6f \n ", positiveCount / n); 
+        System.out.printf("%1.6f \n ", negativeCount / n); 
+        System.out.printf("%1.6f \n ", zeroCount / n); 
+        System.out.println(); 
+    } 
+  
+   
+ 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+}
