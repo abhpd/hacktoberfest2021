b09a6ae5abd8af8a654584ce2c0a82d8c7dad14d
SARVESHKHANDELWAL <sarveshk21122001@gmail.com>
2020-10-11 16:24:13 +0530

    Added Recursion_nth fibonacci

--- C/Algorithms/Recursion/Recursion (nth fibonacci).c
+++ C/Algorithms/Recursion/Recursion (nth fibonacci).c
@@ -1,8 +1,7 @@
 //Fibonacci
-
 #include <stdio.h>
 int fib(int n)
-    {
+{
     int t0=0,t1=1,s=0,i;
 
     if(n<=1) return n;
@@ -17,27 +16,27 @@ int fib(int n)
     return s;
 }
 int rfib(int n)
-    {
+{
     if(n<=1)return n;
     return rfib(n-2)+rfib(n-1);
-    }
+}
     int F[10];
     int mfib(int n)
     {
-    if(n<=1)
-    {
-    F[n]=n;
-    return n;
-    }
+        if(n<=1)
+        {
+        F[n]=n;
+        return n;
+        }
     else
-    {
-    if(F[n-2]==-1)
-    F[n-2]=mfib(n-2);
-    if(F[n-1]==-1)
-    F[n-1]=mfib(n-1);
-    F[n]=F[n-2]+F[n-1];
-    return F[n-2]+F[n-1];
-    }
+        {
+        if(F[n-2]==-1)
+        F[n-2]=mfib(n-2);
+        if(F[n-1]==-1)
+        F[n-1]=mfib(n-1);
+        F[n]=F[n-2]+F[n-1];
+        return F[n-2]+F[n-1];
+        }
 }
 int main()
     {
