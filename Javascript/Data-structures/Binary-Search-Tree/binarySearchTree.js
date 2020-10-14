4b29907cfaee19fbcdc2b69049517e6cbc00c1fc
Gisela <gisela.difini@4all.com>
2020-10-14 08:40:18 -0300

    Add Binary Search Tree

---
+++ Javascript/Data-structures/Binary-Search-Tree/binarySearchTree.js
@@ -0,0 +1,82 @@
+class Node {
+  constructor(value){
+      this.value = value;
+      this.left = null;
+      this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+      this.root = null;
+  }
+  insert(value){
+      var newNode = new Node(value);
+      if(this.root === null){
+          this.root = newNode;
+          return this;
+      }
+      var current = this.root;
+      while(true){
+          if(value === current.value) return undefined;
+          if(value < current.value){
+              if(current.left === null){
+                  current.left = newNode;
+                  return this;
+              }
+              current = current.left;
+          } else {
+              if(current.right === null){
+                  current.right = newNode;
+                  return this;
+              }
+              current = current.right;
+          }
+      }
+  }
+  find(value){
+      if(this.root === null) return false;
+      var current = this.root,
+          found = false;
+      while(current && !found){
+          if(value < current.value){
+              current = current.left;
+          } else if(value > current.value){
+              current = current.right;
+          } else {
+              found = true;
+          }
+      }
+      if(!found) return undefined;
+      return current;
+  }
+  contains(value){
+      if(this.root === null) return false;
+      var current = this.root,
+          found = false;
+      while(current && !found){
+          if(value < current.value){
+              current = current.left;
+          } else if(value > current.value){
+              current = current.right;
+          } else {
+              return true;
+          }
+      }
+      return false;
+  }
+}
+
+
+//      9
+//   5     13
+// 3  8  11  22
+
+var BST = new BinarySearchTree();
+BST.insert(9)
+BST.insert(5)
+BST.insert(13)
+BST.insert(11)
+BST.insert(3)
+BST.insert(22)
+BST.insert(8)
