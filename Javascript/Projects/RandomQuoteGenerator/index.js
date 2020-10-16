d35a45fcab8def4ba32153c05e5fbfa0b6a175ec
Meena <rgmeena1404@gmail.com>
2020-10-16 12:21:07 +0200

    Added Random Quote Generator project javascript

---
+++ Javascript/Projects/RandomQuoteGenerator/index.js
@@ -0,0 +1,68 @@
+var currentQuote = "",
+  currentAuthor = "";
+function openURL(url) {
+  window.open(
+    url,
+    "Share",
+    "width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0"
+  );
+}
+
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: "application/json",
+    },
+
+    url:
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === "string") {
+        quotesData = JSON.parse(jsonQuotes);
+      }
+    },
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+  $("#tweet-quote").attr(
+    "href",
+    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $(".quote-text").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $("#text").text(randomQuote.quote);
+  });
+
+  $(".quote-author").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $("#author").html(randomQuote.author);
+  });
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $("#new-quote").on("click", getQuote);
+
+  $("#tweet-quote").on("click", function () {
+    openURL(
+      "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+        encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+    );
+  });
+});
