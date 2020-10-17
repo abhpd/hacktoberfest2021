a0b26f96eb12b04470cebde808da3a1f7055062b
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2020-10-17 22:34:59 +0530

    Create Bubble_Sort.java

---
+++ Java/Algorithms/Sorting/BubbleSort/Bubble_Sort.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void bubbleSort(int[] arr) {
+    //write your code here
+    for(int i=1;i<= arr.length-1;i++){
+        for(int j=0;j<arr.length-i;j++){
+        if(isSmaller(arr,j+1,j) == true){
+            swap(arr,j+1,j);
+        }
+        }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if ith element is smaller than jth element
+  public static boolean isSmaller(int[] arr, int i, int j) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    bubbleSort(arr);
+    print(arr);
+  }
+
+}
