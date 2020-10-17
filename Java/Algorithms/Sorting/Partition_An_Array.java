738c12a0aa64c5641022a2a94f1344679ac6bca2
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2020-10-17 22:49:34 +0530

    Create Partition_An_Array.java

---
+++ Java/Algorithms/Sorting/Partition_An_Array.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void partition(int[] arr, int pivot){
+    //write your code here
+    int i=0;
+    int j=0;
+    
+    while(i<arr.length){
+        if(arr[i] > pivot){
+            i++;
+        }else{
+            swap(arr,i,j);
+            i++;
+            j++;
+        }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int pivot = scn.nextInt();
+    partition(arr,pivot);
+    print(arr);
+  }
+
+}
