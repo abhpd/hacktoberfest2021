2e696e744c0415927cba67eabfe5f5702fbd1764
Gurpreet Singh Sahni <47444537+gurpreetsahni@users.noreply.github.com>
2020-10-18 13:05:22 +0530

    Update Remove space in the string.cpp

--- C++/DataStructures/Remove space in the string.cpp
+++ C++/DataStructures/Remove space in the string.cpp
@@ -1,3 +1,6 @@
+/* Remove space in the input string , Input is in form- Firts line : Number of Test Cases
+                                                       Second line : String for each test case */
+
 #include <iostream>
 using namespace std;
 
