b536cd5dd5038d75da025d7bc959570a006579bd
Aaryan376 <61610837+Aaryan376@users.noreply.github.com>
2020-10-19 18:21:32 +0530

    0-1 Knapsack Problem solution

---
+++ C++/Algorithms/Dyanmic Programming/0-1 knapsack.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+  
+   int i, w; 
+    int K[n + 1][W + 1]; 
+    for (i = 0; i <= n; i++) { 
+      for (w = 0; w <= W; w++) { 
+         if (i == 0 || w == 0) 
+                K[i][w] = 0; 
+            else if (wt[i - 1] <= w) 
+                K[i][w] = max( val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]); 
+            else
+                K[i][w] = K[i - 1][w]; 
+        } 
+    } 
+  
+    return K[n][W]; 
+} 
+  
+int main() 
+{ 
+    int n,i;
+    cin>>n;
+    int val[n];
+    int wt[n];
+    for(i=0;i<n;i++){
+        cin>>val[i];
+    }
+    
+    for(i=0;i<n;i++){
+        cin>>wt[i];
+    }
+    int W;
+    cin>>W;
+    
+    cout << knapSack(W, wt, val, n); 
+    return 0; 
+} 
