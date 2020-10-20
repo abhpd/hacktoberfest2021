3319e477fb8087efea6722b417551313c0c7c536
Shreyash Chooudhary <37010919+shreyashji@users.noreply.github.com>
2020-10-20 13:25:17 +0530

    updated jump search

--- C++/Algorithms/Searching/Jump Search.cpp
+++ C++/Algorithms/Searching/Jump Search.cpp
@@ -58,3 +58,14 @@ int main()
 Output:
 
 Number 55 is at index 10
+  
+/*Time Complexity : O(√n)
+Auxiliary Space : O(1)
+
+Works only sorted arrays.
+The optimal size of a block to be jumped is (√ n). This makes the time complexity of Jump Search O(√ n).
+The time complexity of Jump Search is between Linear Search ( ( O(n) ) and Binary Search ( O (Log n) ).
+Binary Search is better than Jump Search, but Jump search has an advantage that we traverse back only once
+(Binary Search may require up to O(Log n) jumps, consider a situation where the element to be searched is the smallest element or smaller than the smallest). 
+So in a system where binary search is costly, we use Jump Search.
+*/
