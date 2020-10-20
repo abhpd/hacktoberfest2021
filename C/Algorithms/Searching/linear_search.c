ba7973858b23241391778d784250e44c3f6e9b76
Supuna Kaumada <malliyawadusupuna@gmail.com>
2020-10-20 13:17:50 +0530

    Added Linear search algorithm in c
    Added Search algorithms folder.
    
    Implemented linear search in c language

---
+++ C/Algorithms/Searching/linear_search.c
@@ -0,0 +1,27 @@
+// C code to linearly search x in arr[]. If x
+
+#include <stdio.h>
+
+int search(int arr[], int n, int x)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		if (arr[i] == x)
+			return i;
+	return -1;
+}
+
+// Driver code
+int main(void)
+{
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	// Function call
+	int result = search(arr, n, x);
+	(result == -1)
+		? printf("Element is not present in array")
+		: printf("Element is present at index %d", result);
+	return 0;
+}
