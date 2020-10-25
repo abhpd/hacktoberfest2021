f8ef711d9ac7dc873c3bf817a60c548cc91a96bc
Bozhidar Kirev <nekyvmail@mail.bg>
2020-10-25 18:43:21 +0200

    Update Djikastra.cs
    Added comments

--- CSharp/Algorithms/Djikastra.cs
+++ CSharp/Algorithms/Djikastra.cs
@@ -14,8 +14,14 @@ namespace DijkstraAlgorithm
         This algorithm helps to find the shortest path from a point in a graph (the source) to a destination.
         */
         
+        // A utility function to find the 
+        // vertex with minimum distance 
+        // value, from the set of vertices 
+        // not yet included in shortest 
+        // path tree
         private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
         {
+            // Initialize min value 
             int min = int.MaxValue;
             int minIndex = 0;
  
@@ -31,6 +37,8 @@ namespace DijkstraAlgorithm
             return minIndex;
         }
  
+        // A utility function to print 
+        // the constructed distance array 
         private static void Print(int[] distance, int verticesCount)
         {
             Console.WriteLine("Vertex    Distance from source");
@@ -39,32 +47,63 @@ namespace DijkstraAlgorithm
                 Console.WriteLine("{0}\t  {1}", i, distance[i]);
         }
  
+        // Function that implements Dijkstra's 
+        // single source shortest path algorithm 
+        // for a graph represented using adjacency 
+        // matrix representation 
         public static void DijkstraAlgo(int[,] graph, int source, int verticesCount)
         {
-            int[] distance = new int[verticesCount];
+            int[] distance = new int[verticesCount]; // The output array. distance[i] 
+            // will hold the shortest 
+            // distance from src to i 
+         
+            // shortestPathTreeSet will true if vertex 
+            // i is included in shortest path 
+            // tree or shortest distance from 
+            // src to i is finalized 
             bool[] shortestPathTreeSet = new bool[verticesCount];
  
+            // Initialize all distances as 
+            // INFINITE and shortestPathTreeSet[] as false 
             for (int i = 0; i < verticesCount; ++i)
             {
                 distance[i] = int.MaxValue;
                 shortestPathTreeSet[i] = false;
             }
  
+            // Distance of source vertex 
+            // from itself is always 0
             distance[source] = 0;
- 
+            
+            // Find shortest path for all vertices
             for (int count = 0; count < verticesCount - 1; ++count)
             {
+                // Pick the minimum distance vertex 
+                // from the set of vertices not yet 
+                // processed. u is always equal to 
+                // src in first iteration. 
                 int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+             
+                // Mark the picked vertex as processed
                 shortestPathTreeSet[u] = true;
  
+                // Update dist value of the adjacent 
+                // vertices of the picked vertex.
                 for (int v = 0; v < verticesCount; ++v)
+                    // Update distance[v] only if is not in 
+                    // shortestPathTreeSet, there is an edge from u 
+                    // to v, and total weight of path 
+                    // from src to v through u is smaller 
+                    // than current value of distance[v] 
                     if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distance[u] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
                         distance[v] = distance[u] + graph[u, v];
             }
- 
+            
+            // print the constructed distance array 
             Print(distance, verticesCount);
         }
  
+        // Driver Code
         public static void Main(string[] args)
         {
             int[,] graph =  {
