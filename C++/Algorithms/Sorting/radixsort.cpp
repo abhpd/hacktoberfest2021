e6f662e1bd593e6b0571079319abf7e502c0bdf0
Archiita <architasarkar10@gmail.com>
2020-10-30 12:10:28 +0530

    Added radixsort.cpp

---
+++ C++/Algorithms/Sorting/radixsort.cpp
@@ -0,0 +1,60 @@
+// C++ implementation of Radix Sort 
+#include<iostream> 
+using namespace std; 
+
+int getMax(int arr[], int n) 
+{ 
+	int mx = arr[0]; 
+	for (int i = 1; i < n; i++) 
+		if (arr[i] > mx) 
+			mx = arr[i]; 
+	return mx; 
+} 
+
+void countSort(int arr[], int n, int exp) 
+{ 
+	int output[n];
+	int i, count[10] = {0}; 
+
+	for (i = 0; i < n; i++) 
+		count[ (arr[i]/exp)%10 ]++; 
+
+
+	for (i = 1; i < 10; i++) 
+		count[i] += count[i - 1]; 
+
+	for (i = n - 1; i >= 0; i--) 
+	{ 
+		output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+		count[ (arr[i]/exp)%10 ]--; 
+	} 
+ 
+	for (i = 0; i < n; i++) 
+		arr[i] = output[i]; 
+} 
+
+ 
+void radixsort(int arr[], int n) 
+{ 
+
+	int m = getMax(arr, n); 
+
+	for (int exp = 1; m/exp > 0; exp *= 10) 
+		countSort(arr, n, exp); 
+} 
+void print(int arr[], int n) 
+{ 
+	for (int i = 0; i < n; i++) 
+		cout << arr[i] << " "; 
+} 
+ 
+int main() 
+{ 
+	int n;
+	cin >> n;
+	int a[1000];
+	for(int i = 0; i < n; i++) cin >> a[i];
+	radixsort(a, n); 
+	print(a, n); 
+	return 0; 
+} 
