cc31f6291adb9411be5e9a9d753a8891d5467200
Naman1809 <72972766+Naman1809@users.noreply.github.com>
2021-10-01 18:30:40 +0530

    Create kreverse.cpp

---
+++ C++/DataStructures/Linkedlist/kreverse.cpp
@@ -0,0 +1,103 @@
+/*
+Given a singly linked list of integers, reverse the nodes of the linked list 'k' at a time and return its modified list.
+ 'k' is a positive integer and is less than or equal to the length of the linked list.
+ If the number of nodes is not a multiple of 'k,' then left-out nodes, in the end, should be reversed as well.
+*/
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+
+Node *kReverse(Node *head, int k)
+{
+    if (!head)
+        return NULL;
+    if(k==0)
+    {
+        return head;
+    }
+    Node* current = head;
+    Node* next = NULL;
+    Node* prev = NULL;
+    int count = 0;
+ 
+    /*reverse first k nodes of the linked list */
+    while (current != NULL && count < k) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        count++;
+    }
+ 
+    /* next is now a pointer to (k+1)th node
+    Recursively call for the list starting from current.
+    And make rest of the list as next of first node */
+    if (next != NULL)
+        head->next = kReverse(next, k);
+ 
+    /* prev is new head of the input list */
+    return prev;
+}
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int k;
+		cin >> k;
+		head = kReverse(head, k);
+		print(head);
+	}
+	
+	return 0;
+}
