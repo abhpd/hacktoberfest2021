16d0a208154bcf63a6d8ef9e972d3b59c4416989
Philip Schikora <pschikora87@googlemail.com>
2021-10-02 05:37:44 +0200

    add Java BucketSort

---
+++ Java/Algorithms/Sorting/BucketSort/BucketSort.java
@@ -0,0 +1,47 @@
+import java.util.*; import java.util.Collections;
+ 
+class BucketSort {
+ 
+    public static void main(String args[])
+    {
+        float arr[] = { (float)0.897, (float)0.565,
+                        (float)0.656, (float)0.1234,
+                        (float)0.665, (float)0.3434 };
+ 
+        int n = arr.length;
+        sort(arr, n);
+ 
+        System.out.println("Sorted array is ");
+        for (float el : arr) {
+            System.out.print(el + " ");
+        }
+    }
+    static void sort(float arr[], int n)
+    {
+        if (n <= 0) {
+            return;
+        }
+        @SuppressWarnings("unchecked")
+        Vector<Float>[] buckets = new Vector[n];
+ 
+        for (int i = 0; i < n; i++) {
+            buckets[i] = new Vector<Float>();
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            float idx = arr[i] * n;
+            buckets[(int)idx].add(arr[i]);
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            Collections.sort(buckets[i]);
+        }
+ 
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < buckets[i].size(); j++) {
+                arr[index++] = buckets[i].get(j);
+            }
+        }
+    }
+}
