84f3562ce2429dbd8c4da92fcb0b66a9154c877c
PRABHU PRASAD PENTHOI <72146816+PRABHU-OFFICIAL@users.noreply.github.com>
2021-10-02 13:29:08 +0530

    Update Selection sort.java

--- Python/Algorithms/Sorting/Selection_sort.py
+++ Python/Algorithms/Sorting/Selection_sort.py
@@ -1,15 +1,17 @@
-#selection sort
-def selection_sort(alist):
-    for i in range(0, len(alist) - 1):
-        smallest = i
-        for j in range(i + 1, len(alist)):
-            if alist[j] < alist[smallest]:
-                smallest = j
-        alist[i], alist[smallest] = alist[smallest], alist[i]
- 
- 
-alist = input('Enter the list of numbers: ').split()
-alist = [int(x) for x in alist]
-selection_sort(alist)
-print('Sorted list: ', end='')
-print(alist)
+import sys
+A = [64, 25, 12, 22, 11]
+
+for i in range(len(A)):
+
+	min_idx = i
+	for j in range(i+1, len(A)):
+		if A[min_idx] > A[j]:
+			min_idx = j
+			
+
+	A[i], A[min_idx] = A[min_idx], A[i]
+
+
+print ("Sorted array")
+for i in range(len(A)):
+	print("%d" %A[i]),
