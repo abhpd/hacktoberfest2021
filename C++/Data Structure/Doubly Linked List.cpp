0440bd46f385f73bf4b02ec9d3294d4994bd0112
Shivam Gupta <65845496+thisisshivamgupta@users.noreply.github.com>
2021-10-02 15:11:02 +0530

    Implementation of Doubly Linked List

---
+++ C++/Data Structure/Doubly Linked List.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+struct Node {
+   int data;
+   struct Node *prev;
+   struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int newdata) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = newdata;
+   newnode->prev = NULL;
+   newnode->next = head;
+   if(head != NULL)
+   head->prev = newnode ;
+   head = newnode;
+}
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   while(ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+int main() {
+   insert(12);
+   insert(71);
+   insert(17);
+   insert(21);
+   insert(69);
+   cout<<"The doubly linked list is: ";
+   display();
+   return 0;
+}
