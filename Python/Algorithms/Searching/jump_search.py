dcb8673080220068e8c5ebb980b8c3399f2f1271
PRABHU PRASAD PENTHOI <72146816+PRABHU-OFFICIAL@users.noreply.github.com>
2021-10-02 13:34:59 +0530

    Update jump_search.py

--- Python/Algorithms/Searching/jump_search.py
+++ Python/Algorithms/Searching/jump_search.py
@@ -1,46 +1,35 @@
-# Python3 code to implement Jump Search 
-import math 
+import math
 
-def jumpSearch( arr , x , n ): 
+def jumpSearch( arr , x , n ):
 	
-	# Finding block size to be jumped 
-	step = math.sqrt(n) 
-	
-	# Finding the block where element is 
-	# present (if it is present) 
+	step = math.sqrt(n)
+
 	prev = 0
-	while arr[int(min(step, n)-1)] < x: 
-		prev = step 
-		step += math.sqrt(n) 
-		if prev >= n: 
+	while arr[int(min(step, n)-1)] < x:
+		prev = step
+		step += math.sqrt(n)
+		if prev >= n:
 			return -1
-	
-	# Doing a linear search for x in 
-	# block beginning with prev. 
-	while arr[int(prev)] < x: 
+
+	while arr[int(prev)] < x:
 		prev += 1
 		
-		# If we reached next block or end 
-		# of array, element is not present. 
-		if prev == min(step, n): 
+		if prev == min(step, n):
 			return -1
 	
-	# If element is found 
-	if arr[int(prev)] == x: 
-		return prev 
+	
+	if arr[int(prev)] == x:
+		return prev
 	
 	return -1
 
-arr = []
-inp = input("Enter the array: ")
-inp = list(inp.split())
-arr = list(map(int, inp))
-x = int(input("Enter the element you want to search: "))
-n = len(arr) 
 
-# Find the index of 'x' using Jump Search 
-index = jumpSearch(arr, x, n) 
+arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21,
+	34, 55, 89, 144, 233, 377, 610 ]
+x = 55
+n = len(arr)
 
-# Print the index where 'x' is located 
-print("Number" , x, "is at index" ,"%.0f"%index) 
- 
+
+index = jumpSearch(arr, x, n)
+
+print("Number" , x, "is at index" ,"%.0f"%index)
