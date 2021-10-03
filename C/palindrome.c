c7560b1e1129e93df950dfe6a64accf7b0fc8824
Varun Rai <77796938+kgrai@users.noreply.github.com>
2021-10-03 23:16:47 +0530

    palindrome.c

---
+++ C/palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("%d is a palindrome number",temp);    
+else    
+printf("%d is not a palindrome",temp);   
+return 0;  
+} 
