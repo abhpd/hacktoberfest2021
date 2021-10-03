12f5295bbaa5c81bcee935755d05c8c8ff7d074d
Annapurna Shobitha S <74901536+annapurna2003@users.noreply.github.com>
2021-10-03 16:36:43 +0530

    pencil sketch

---
+++ Python/pencil sketch/pencil_sketch.py
@@ -0,0 +1,10 @@
+import cv2
+image = cv2.imread(r"C:\Users\ANNAPURNA\Downloads\annu.png") #reading the image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #converting it to grayscale
+inverted_image = 255 - gray_image
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+inverted_blurred = 255 - blurred #creating pencil sketch
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=250.0)
+cv2.imshow("Original Image", image)
+cv2.imshow("Pencil Sketch of annapurna", pencil_sketch)
+cv2.waitKey(0)
