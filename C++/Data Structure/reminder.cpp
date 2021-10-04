7a4a0612746735c88c2e8a0978a9da1e22a37b78
Siddhant <88947193+SiddhantManze@users.noreply.github.com>
2021-10-05 00:49:53 +0530

    Create reminder.cpp

---
+++ C++/Data Structure/reminder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        cout<<a%b<<endl;
+    }
+    return 0;
+}
