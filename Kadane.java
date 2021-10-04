08581106d7a6839a9c5d954359d60b824ac65566
vaibhavss08 <81666621+vaibhavss08@users.noreply.github.com>
2021-10-04 12:41:08 +0530

    Create Kadane.java

---
+++ Kadane.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class Kadane {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		  
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    System.out.println(maxSubarraySum(arr,n));
+		}
+  }
+    
+    public static int maxSubarraySum(int arr[], int n){
+        
+        int msum=0;
+        int csum=0;
+        
+        for(int i=0;i<n;i++){
+            csum+=arr[i];
+            
+            if(csum>msum){
+                msum=csum;
+            }
+            
+            if(csum<0){
+                csum=0;
+            }
+        }
+        return msum;
+        
+    }
+}
+
