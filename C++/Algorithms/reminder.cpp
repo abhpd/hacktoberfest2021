17acf207b0949780a842cab242c560374611228c
Siddhant <88947193+SiddhantManze@users.noreply.github.com>
2021-10-05 00:53:55 +0530

    Create reminder.cpp

---
+++ C++/Algorithms/reminder.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        cout<<a%b<<endl;
+    }
+    return 0;
+}
