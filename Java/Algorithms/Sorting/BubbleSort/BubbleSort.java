65d1a10ecd1e4421f5f159ac9e1fbc4a64b0ad5f
NayanVR <nvradadiya281@gmail.com>
2021-10-05 22:37:24 +0530

    Optimized Java bubble sort

--- Java/Algorithms/Sorting/BubbleSort/BubbleSort.java
+++ Java/Algorithms/Sorting/BubbleSort/BubbleSort.java
@@ -7,12 +7,12 @@ public class BubbleSort {
   public static void main(String[] args) {
     int[] originalArray = new int[10];
 
-    //Here Im making
+    // Here Im making
     for (int i = 0; i < originalArray.length; i++) {
       originalArray[i] = (int) (Math.random() * 15);
     }
 
-    bubbleSort(originalArray);    
+    bubbleSort(originalArray);
 
     for (int i = 0; i < originalArray.length; i++) {
       System.out.println(originalArray[i]);
@@ -20,14 +20,26 @@ public class BubbleSort {
   }
 
   public static void bubbleSort(int[] a) {
+
+    boolean swapped;
     int temp;
+
     for (int j = 0; j < a.length; j++) {
+
+      swapped = false;
+
       for (int i = 0; i < a.length - 1; i++) {
         if (a[i] >= a[i + 1]) {
           temp = a[i];
           a[i] = a[i + 1];
           a[i + 1] = temp;
         }
+      }
+
+      // if you did not swap for a particular value of i, it means the array is sorted
+      // hence stop the program
+      if (!swapped) {
+        break;
       }
     }
   }
