7ad5cb41eab4bac367782d095eb741a5ac5b30c0
KishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2021-10-05 15:01:30 +0530

    Adding MergeSort and ShellSort In Sorting section In Python

---
+++ Python/Algorithms/Sorting/ShellSort/ShellSort.py
@@ -0,0 +1,34 @@
+#Function for shellSort
+def shellSort(arr):
+  
+    n = len(arr)
+    gap = n/2
+  
+    while gap > 0:
+  
+        for i in range(gap,n):
+  
+            temp = arr[i]
+  
+            j = i
+            while  j >= gap and arr[j-gap] >temp:
+                arr[j] = arr[j-gap]
+                j -= gap
+  
+            arr[j] = temp
+        gap /= 2
+  
+  
+# Driver code
+arr = [ 12, 34, 54, 2, 3]
+  
+n = len(arr)
+print ("Array before sorting:")
+for i in range(n):
+    print(arr[i]),
+  
+shellSort(arr)
+  
+print ("\nArray after sorting:")
+for i in range(n):
+    print(arr[i])
