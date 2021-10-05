d7d0556e305bcc347ba59a3d1fd3dcd9b63a4ea7
adwait41001 <57260468+adwait41001@users.noreply.github.com>
2021-10-05 17:42:36 +0530

    Create DiscordRedditBot.js

---
+++ Javascript/Projects/DiscordRedditBot.js
@@ -0,0 +1,34 @@
+// Author : Adwait Patil
+const Discord = require('discord.js');
+const api = require("imageapi.js");
+const keepAlive = require("./server")
+const fetch = require("node-fetch")
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: 'with your mind',
+        }
+    });
+});
+
+const reddit = 'https://www.reddit.com/r/';
+const memetext = 'A meme from r/'
+client.on('message', async msg => {
+  if ((msg.content === 'meme')) {
+    let subreddits = [
+      "memes","dankmemes"
+    ];
+    let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length))];
+    let img = await api(subreddit)
+    const Embed = new Discord.MessageEmbed()
+    .setTitle(memetext.concat(subreddit))
+    .setURL(reddit.concat(subreddit))
+    .setColor('RANDOM')
+    .setImage(img)
+    msg.channel.send(Embed)
+  }
+});
+client.login(token) //use your own token here.
