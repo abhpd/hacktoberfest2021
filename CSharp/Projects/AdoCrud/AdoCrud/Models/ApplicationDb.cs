56e725b8d0c3ba3aca8ca9d527d785f804bfe15a
safal08 <safalmahat5@gmail.com>
2021-10-06 09:26:09 +0545

    ADO Crud added

---
+++ CSharp/Projects/AdoCrud/AdoCrud/Models/ApplicationDb.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdoCrud.Models
+{
+    public class ApplicationDb
+    {
+        private readonly IConfiguration _config;
+
+
+        public ApplicationDb(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        static string conString = null;
+        public string GetConString()
+        {
+            conString = _config.GetConnectionString("TestConnection").ToString();
+            return conString;
+        }
+
+        //static string conString = @"Data Source=DESKTOP-KBAPQC1;Initial Catalog=DonationDB;Integrated Security=True";
+        SqlConnection con = new SqlConnection(conString);
+
+        public DataSet CandidateGet(Candidate can, out string msg)
+        {
+            DataSet ds = new DataSet();
+            msg = "";
+            try
+            {
+                SqlCommand com = new SqlCommand("spCandidate", con);
+                com.CommandType = CommandType.StoredProcedure;
+
+                com.Parameters.AddWithValue("@Id", can.Id);
+                com.Parameters.AddWithValue("@FullName", can.FullName);
+                com.Parameters.AddWithValue("@Mobile", can.Mobile);
+                com.Parameters.AddWithValue("@Email", can.Email);
+                com.Parameters.AddWithValue("@Age", can.Age);
+                com.Parameters.AddWithValue("@BloodGroup", can.BloodGroup);
+                com.Parameters.AddWithValue("@Address", can.Address);
+                com.Parameters.AddWithValue("@Flag", can.Flag);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+
+                msg = ex.Message;
+                return ds;                
+            }
+            
+
+        }
+
+    }
+}
---
+++ CSharp/Projects/AdoCrud/AdoCrud/Models/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdoCrud.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string BloodGroup { get; set; }
+        public string Address { get; set; }
+        public string Flag { get; set; }
+
+
+    }
+}
