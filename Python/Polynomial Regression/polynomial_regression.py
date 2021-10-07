9f06f505266e1f7c87e9c94daa077fd1bb0d482f
Sreejith S Pillai <47489986+sreejith97@users.noreply.github.com>
2021-10-07 23:12:50 +0530

    Update polynomial_regression.py

--- Python/Polynomial Regression/polynomial_regression.py
+++ Python/Polynomial Regression/polynomial_regression.py
@@ -40,6 +40,7 @@ plt.title('Truth or Bluff (Linear Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
+lin_reg.fit(X, y)
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
@@ -54,6 +55,14 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
