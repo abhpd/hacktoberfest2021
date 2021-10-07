34cd03ad4534c7f95b4b7bcc5a1cb291247342af
prince0786 <57414351+prince0786@users.noreply.github.com>
2021-10-07 09:58:45 +0530

    Queue using Array.py

---
+++ Python/Algorithms/Queue using Array.py
@@ -0,0 +1,18 @@
+#empty array declaration
+arr = []
+
+#built-in list function that adds three elements to the array.
+arr.append(1)
+arr.append(2)
+arr.append(3)
+
+print("printing array...")
+print(arr)
+
+#built-in list function that removes elements from an array
+print("removing two elements from array...")
+arr.pop(0)
+arr.pop(0)
+
+print("printing the remaining element(s) in array...")
+print(arr)
