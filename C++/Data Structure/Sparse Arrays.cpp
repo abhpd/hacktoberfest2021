2f3aefc67915cf4894ef10370fef487a7c4bd374
geek-nripesh <73373839+geek-nripesh@users.noreply.github.com>
2021-10-08 21:05:54 +0530

    Create Sparse Arrays.cpp
    Hackerrank question : Sparse Arrays

---
+++ C++/Data Structure/Sparse Arrays.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+    
+    int m = strings.size(), n = queries.size();
+    vector<int> v;
+    int count;
+    for(int i=0; i<n; i++){
+        count = 0;
+        for(int j=0; j<m; j++){
+            if(queries[i]==strings[j]){
+                count++;
+            }
+        }
+        v.push_back(count);
+    }
+    
+    return v;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string strings_count_temp;
+    getline(cin, strings_count_temp);
+
+    int strings_count = stoi(ltrim(rtrim(strings_count_temp)));
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
+    }
+
+    string queries_count_temp;
+    getline(cin, queries_count_temp);
+
+    int queries_count = stoi(ltrim(rtrim(queries_count_temp)));
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (size_t i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
