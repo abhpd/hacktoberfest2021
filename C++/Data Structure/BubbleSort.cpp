49b7f61e6c7f4643176ade3f6632c280c11b4879
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2021-10-08 22:57:53 +0530

    Created Bubblesort.cpp

---
+++ C++/Data Structure/BubbleSort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *ab, int *cd)
+{
+    int temp = *ab;
+    *ab = *cd;
+    *cd = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)    
+
+    // Last i elements are already in place
+    for (j = 0; j < n-i-1; j++)
+        if (arr[j] > arr[j+1])
+            swap(&arr[j], &arr[j+1]);
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {44, 34, 47,1, 89, 32, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    cout<<"Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
