d67d4c4580dc899be4206d9ac5d03c8db2d3f80e
geek-nripesh <73373839+geek-nripesh@users.noreply.github.com>
2021-10-08 21:06:57 +0530

    Create Print the Elements of a Linked List.cpp
    HackerRank question : Print the Elements of a Linked List

---
+++ C++/Data Structure/Print the Elements of a Linked List.cpp
@@ -0,0 +1,20 @@
+
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+    while(head != NULL){
+        cout<<head->data<<endl;
+        head = head->next; 
+    }
+}
+
