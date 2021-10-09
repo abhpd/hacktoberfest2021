a4839aba35c4e92e1ca347e3a5536bfae7666e58
Himadri Nayak <himadri.nayak1@gmail.com>
2021-10-09 14:58:36 +0530

    reverse linked list code

--- C/LinkedList/Reverse-Linked-List.c
+++ C/LinkedList/Reverse-Linked-List.c
@@ -27,17 +27,11 @@ static void reverse(struct Node** head_ref)
 /* Function to push a node */
 void push(struct Node** head_ref, int new_data)
 {
-    /* allocate node */
     struct Node* new_node =
             (struct Node*) malloc(sizeof(struct Node));
              
-    /* put in the data  */
     new_node->data  = new_data;
-                 
-    /* link the old list off the new node */
     new_node->next = (*head_ref);    
-         
-    /* move the head to point to the new node */
     (*head_ref)    = new_node;
 }
   
