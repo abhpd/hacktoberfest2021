e52dfd2ad4bb00cfb05a07d9c31b307777785324
Madan <58905767+Madanaa@users.noreply.github.com>
2021-10-09 10:34:36 -0700

    Update ReplaceElement.java

--- Java/Algorithms/Arrays/ReplaceElement.java
+++ Java/Algorithms/Arrays/ReplaceElement.java
@@ -1,1 +1,31 @@
 
+import java.io.*; 
+import java.util.*; 
+
+
+class ReplaceElement {
+	
+            public static int i;
+
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  int N = sc.nextInt();
+  int[] na = new int[N];
+  int[] a = new int[N];
+  for (int i = 0; i < N; i++)
+   a[i] = sc.nextInt();
+  for (i = 0; i < N; i++) {
+   if (i == 0) {
+    na[i] = a[i] * a[i + 1];
+   }
+   else {
+    if (i == N - 1) {
+     na[i] = a[i] * a[i - 1];
+    } else
+     na[i] = a[i - 1] * a[i + 1];
+   }
+  }
+  for (i = 0; i < N; i++)
+   System.out.print(na[i] + " ");
+ }
+}
