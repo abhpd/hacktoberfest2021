daa9c8acb8fa03ed8a8f3b266988e7d453a3ecd6
geekytaurus115 <geekytaurus115@gmail.com>
2021-10-11 00:04:50 +0530

    JS Project Guess-My-Number-Game added

---
+++ Javascript/Projects/guess-my-number/README.md
@@ -0,0 +1,5 @@
+#### This is a number guessing game project using HTML, CSS and JavaScript.
+<pre>
+ ---> Each user will get a message too high or too low or correct number!
+</pre>
+
---
+++ Javascript/Projects/guess-my-number/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  //when there is no input
+  if (!guess) {
+    displayMessage("🧐 No number!");
+
+    //when player win
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    //update highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too Low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = 20;
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
