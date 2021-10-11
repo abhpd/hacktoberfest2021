33be67c7bc4dfa3da4936a5dfa2e642a4c0b77ae
Sumanth Bajjuri <73301202+SumanthBajjuri07@users.noreply.github.com>
2021-10-12 02:35:36 +0530

    Create BinarySearch02.cpp

---
+++ C++/Algorithms/Searching/BinarySearch02.cpp
@@ -0,0 +1,43 @@
+
+
+
+// CPP program to implement
+// Binary Search in
+// Standard Template Library (STL)
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void show(int a[], int arraysize)
+{
+	for (int i = 0; i < arraysize; ++i)
+		cout << a[i] << ",";
+}
+
+int main()
+{
+	int a[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+	int asize = sizeof(a) / sizeof(a[0]);
+	cout << "\nThe array is : \n";
+	show(a, asize);
+
+	cout << "\n\nLet's say we want to search for ";
+	cout << "\n2 in the array So, we first sort the array";
+	sort(a, a + asize);
+	cout << "\n\nThe array after sorting is : \n";
+	show(a, asize);
+	cout << "\n\nNow, we do the binary search";
+	if (binary_search(a, a + 10, 2))
+		cout << "\nElement found in the array";
+	else
+		cout << "\nElement not found in the array";
+
+	cout << "\n\nNow, say we want to search for 10";
+	if (binary_search(a, a + 10, 10))
+		cout << "\nElement found in the array";
+	else
+		cout << "\nElement not found in the array";
+
+	return 0;
+}
