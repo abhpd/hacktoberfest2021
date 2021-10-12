a8b86202e2fb5a22e0c955e692bc57066d79525e
Ritika <ritikasaxena911@gmail.com>
2021-10-12 19:52:11 +0530

    modified

--- Java/Algorithms/Arrays/ReverseArray.java
+++ Java/Algorithms/Arrays/ReverseArray.java
@@ -8,40 +8,18 @@
 import java.util.*;
 public class ReverseArray{
 	public static void main(String args[]){
-		Solution s = new Solution();
-		int arr[] = {10,5,7,30};
-
-		System.out.println("Array Before Reverse");
-		s.printArray(arr);
-
-		//reverse
-		s.reverseArray(arr);
-
-		System.out.println("Array After Sorting");
-		s.printArray(arr);
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		int ar[]=new int[n];
+	    System.out.println("Enter array elements");
+		for(int i=0;i<n;i++){
+			ar[i]=sc.nextInt();
+		}
+		System.out.println("The reversed array is:");
+		for(int i=(n-1);i>=0;i--){
+			System.out.println(ar[i]+" ");
+		}
 		
-	}
-}
-
-
-class Solution{
-	void reverseArray(int arr[]){
-		int i =0,j = arr.length-1;
-		while(i<j){
-			arr[i]=arr[i]+arr[j];
-			arr[j]=arr[i]-arr[j];
-			arr[i]=arr[i]-arr[j];
-			i++;
-			j--;
-
-		}
-	}
-
-	void printArray(int arr[]){
-		for(int i:arr){
-			System.out.print(i+" ");
-
-		}
-		System.out.println();
 	}
 }
