97ef7a5a8852b01907bdbdd4c69b97b984520d29
Harsh Wardhan Gupta <wardhanguptaharsh@gmail.com>
2021-10-12 21:54:53 +0530

    Added Shell sort in Sorting in Algorithms under C

---
+++ C/Algorithms/Sorting/shell_sort.c
@@ -0,0 +1,35 @@
+// Shell Sort in C programming
+
+#include <stdio.h>
+
+// Shell sort
+void shellSort(int array[], int n) {
+  // Rearrange elements at each n/2, n/4, n/8, ... intervals
+  for (int interval = n / 2; interval > 0; interval /= 2) {
+    for (int i = interval; i < n; i += 1) {
+      int temp = array[i];
+      int j;
+      for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+        array[j] = array[j - interval];
+      }
+      array[j] = temp;
+    }
+  }
+}
+
+// Print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+// Driver code
+int main() {
+  int data[] = {9, 8, 3, 7, 5, 6, 4, 1};
+  int size = sizeof(data) / sizeof(data[0]);
+  shellSort(data, size);
+  printf("Sorted array: \n");
+  printArray(data, size);
+}
