c383a1fb6d2cd2a85004951ab1208e5df19dbe07
Sumant <sumant.sharma.nonu@gmail.com>
2021-10-13 20:53:16 +0530

    change in dp file

--- Javascript/Data-structures/Hash-Table/dynamicProgramming.java
+++ Javascript/Data-structures/Hash-Table/dynamicProgramming.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class dynamicProgramming {
 
     public static void main(String[] args) throws Exception {
-   Scanner scn=new Scanner(System.in);
+
    
    int n=scn.nextInt();
    int arr[]=new int[n];
