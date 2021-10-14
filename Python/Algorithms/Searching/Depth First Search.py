383d43d56fa758e56bcdf1e940a96c46c0d0238e
Rakha Asyrofi <rakhasyrofist@gmail.com>
2021-10-15 01:03:36 +0700

    Update Depth First Search.py

--- Python/Algorithms/Searching/Depth First Search.py
+++ Python/Algorithms/Searching/Depth First Search.py
@@ -1,19 +1,20 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-
-    print(start)
-
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
-
-
+# construct daa
 graph = {'0': set(['1', '2']),
          '1': set(['0', '3', '4']),
          '2': set(['0']),
          '3': set(['1']),
          '4': set(['2', '3'])}
 
-dfs(graph, '0')
+# funct graph
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+        visited.add(start)
+        print(start)
+    else visited is not None:
+         return visited
+
+#loop graf    
+for next in graph[start] - visited:
+    dfs(graph, '0')
+    dfs(graph, next, visited)
