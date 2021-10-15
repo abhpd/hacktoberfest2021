00323812f1f41e7e1e0f0bad75612faabb88c589
Chahak02 <83837855+Chahak02@users.noreply.github.com>
2021-10-15 09:00:43 -0700

    Create ButterflyPattern.java

---
+++ Java/ButterflyPattern.java
@@ -0,0 +1,43 @@
+ class Butterfly_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,k,n=5,m;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++) {
+				System.out.print("*");
+				
+			}
+			
+			for(k=2*(n-i);k>0;k--)
+			{
+				System.out.print(" ");
+			}
+			for(m=i;m>0;m--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(i=n;i>0;i--)
+		{
+			for(j=1;j<=i;j++) {
+				System.out.print("*");
+				
+			}
+			
+			for(k=2*(n-i);k>0;k--)
+			{
+				System.out.print(" ");
+			}
+			for(m=i;m>0;m--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
