0fce6a41f6b50728602dde54acfc8ecae1ef2a5b
Francesco <88930554+FraM1000@users.noreply.github.com>
2021-10-16 16:10:38 +0200

    Update Actualtime.java
    added a logic , asking the client if he wants to know the time

--- Java/Projects/Actualtime.java
+++ Java/Projects/Actualtime.java
@@ -9,8 +9,18 @@ public class Actualtime {
         // Set the date/time format we want.
         DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
+        // Create a Scanner object
+        Scanner scan = new Scanner(System.in); 
+        //asking to the user if he wants to know the time
+        System.out.println("Do you want to know what time is it?" + "\n" + "yes" +"\n" + "no")
+        //reading the input
+        String answer = scan.nextLine();
+        //adding a control so if the user say yes we print the time.
+        if(answer=="yes"){
         // Print the result.
         System.out.println(timeformat.format(now));
+        }
+      
     }
 }
 // By Fonta22
