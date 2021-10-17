1de2f63114db4d0c1d37456f6300fadbaa600729
Rupali Sood <83212983+rupali14-g@users.noreply.github.com>
2021-10-17 11:59:49 +0530

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the value of 'n': "))
+a = 0
+b = 1
+sum = 0
+count = 1
+print("Fibonacci Series: ", end = " ")
+while(count <= n):
+  print(sum, end = " ")
+  count += 1
+  a = b
+  b = sum
+  sum = a + b
