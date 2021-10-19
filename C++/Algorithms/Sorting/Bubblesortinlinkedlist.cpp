637af74cbc1e55d7d589835b706d7a19d05b7134
Jessica Jones <83399901+ichjessicajones@users.noreply.github.com>
2021-10-19 18:50:36 +0530

    Create Bubblesortinlinkedlist.cpp

---
+++ C++/Algorithms/Sorting/Bubblesortinlinkedlist.cpp
@@ -0,0 +1,86 @@
+//bubble sort iterative
+#include <iostream>
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1)
+    {
+        Node *newnode = new Node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node *bubbleSort(Node *head)
+{
+    // Write your code here
+    Node *current = head;
+    Node *index = NULL;
+    int temp;
+    if (head == NULL)
+    {
+        return head;
+    }
+    else
+    {
+        while (current != NULL)
+        {
+            index = current->next;
+            while (index != NULL)
+            {
+                if (current->data > index->data)
+                {
+                    temp = current->data;
+                    current->data = index->data;
+                    index->data = temp;
+                }
+                index = index->next;
+            }
+            current = current->next;
+        }
+        return head;
+    }
+}
+
+int main()
+{
+    Node *head = takeinput();
+    head = bubbleSort(head);
+    print(head);
+}
