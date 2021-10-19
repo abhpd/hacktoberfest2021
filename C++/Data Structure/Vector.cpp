8ff17ee3970f61bd4b4e94d16f35b7a52174e6d1
Ankit Kumar Singh <72161660+ankit-singh706@users.noreply.github.com>
2021-10-19 16:35:51 +0530

    Update Vector.cpp
    Added some modifiers that can used in vectors

--- C++/Data Structure/Vector.cpp
+++ C++/Data Structure/Vector.cpp
@@ -41,3 +41,15 @@ int main() {
 	cout << d.capacity() << endl; // Ans: 5
 	cout << d.max_size() << endl;
 }
+
+
+// Some modifiers that can be used with vectors-
+// assign() – It assigns new value to the vector elements by replacing old ones
+// push_back() – It push the elements into a vector from the back
+// pop_back() – It is used to pop or remove elements from a vector from the back.
+// insert() – It inserts new elements before the element at the specified position
+// erase() – It is used to remove elements from a container from the specified position or range.
+// swap() – It is used to swap the contents of one vector with another vector of same type. Sizes may differ.
+// clear() – It is used to remove all the elements of the vector container
+// emplace() – It extends the container by inserting new element at position
+// emplace_back() – It is used to insert a new element into the vector container, the new element is added to the end of the vector
