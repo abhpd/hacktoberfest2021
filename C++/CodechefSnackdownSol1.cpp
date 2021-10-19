1b0ecfb6cc0ebc188d0df773c70b6849db696b03
SanchitSinghal-SS <72185331+SanchitSinghal-SS@users.noreply.github.com>
2021-10-19 07:28:14 +0530

    Create CodechefSnackdownSol1.cpp

---
+++ C++/CodechefSnackdownSol1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(A==7||B==7||C==7)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
