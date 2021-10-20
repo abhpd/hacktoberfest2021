343f13ca8c158e42c1f9d0afb1ccb588f6ace71f
Vishv Kaneria <72430543+VishvKaneria@users.noreply.github.com>
2021-10-20 10:58:30 +0530

    Create Stack.java

---
+++ Java/Algorithms/Stack.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+ 
+class Test
+{  
+    static void stack_push(Stack<Integer> stack)      // Pushing element on the top of the stack
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            stack.push(i);
+        }
+    }
+     
+    static void stack_pop(Stack<Integer> stack)       // Popping element from the top of the stack
+    {
+        System.out.println("Pop Operation:");
+ 
+        for(int i = 0; i < 5; i++)
+        {
+            Integer y = (Integer) stack.pop();
+            System.out.println(y);
+        }
+    }
+ 
+    static void stack_peek(Stack<Integer> stack)      // Displaying element on the top of the stack
+    {
+        Integer element = (Integer) stack.peek();
+        System.out.println("Element on stack top: " + element);
+    }
+     
+    static void stack_search(Stack<Integer> stack, int element)       // Searching element in the stack
+    {
+        Integer pos = (Integer) stack.search(element);
+ 
+        if(pos == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element is found at position: " + pos);
+    }
+ 
+ 
+    public static void main (String[] args)
+    {
+        Stack<Integer> stack = new Stack<Integer>();
+ 
+        stack_push(stack);
+        stack_pop(stack);
+        stack_push(stack);
+        stack_peek(stack);
+        stack_search(stack, 2);
+        stack_search(stack, 6);
+    }
+}
