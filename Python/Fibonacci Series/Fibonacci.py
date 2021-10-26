0112f2885f48f04231d32f54432bf23c33d1b2bf
Alice2212 <Hezealice@gmail.com>
2021-10-26 01:21:40 +0100

    second commit

--- Python/Fibonacci Series/Fibonacci.py
+++ Python/Fibonacci Series/Fibonacci.py
@@ -1,24 +1,18 @@
 def recur_fibo(n):  
 
-    try:
-        if n <= 1:  
-            return n  
-        else:  
-            return(recur_fibo(n-1) + recur_fibo(n-2)) 
+    if n <= 1:  
+        return n  
+    else:  
+        return(recur_fibo(n-1) + recur_fibo(n-2)) 
 
-        # take input from the user  
-        nterms = int(input("How many terms? "))  
+    # take input from the user  
+    nterms = int(input("How many terms? "))  
 
-        # check if the number of terms is valid 
-
-        if nterms <= 0:  
+    # check if the number of terms is valid 
+    if nterms <= 0:  
         print("Plese enter a positive integer")  
-        else:  
+    else:  
         print("Fibonacci sequence:")  
-        for i in range(nterms):  
-            print(recur_fibo(i)) 
-
-    except:
-
-        print ("Enter a valid integer")
+    for i in range(nterms):  
+        print(recur_fibo(i)) 
 
