411218804ff2c1df9ca8487152998bcb416526dd
Parth Chaudhary <73033665+itsparthchow@users.noreply.github.com>
2021-10-29 17:22:21 +0000

    Create DNF sort.cpp
    DNF sort algorithm

---
+++ C++/Algorithms/Sorting/DNF sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void dnfSort(int arr[], int n){
+  
+  int low=0;
+  int mid=0;
+  int high=n-1;
+  
+  while(mid<=high){
+    if(arr[mid]==0){
+      swap(arr,low,mid);
+      low++;
+      mid++;
+    }
+   else if(arr[mid]==1)
+   {
+     mid++;
+   }
+    else{
+      swap(arr,mid,high);
+      high--;
+    }
+  }
+}
+
+void swap(int arr[], int i, int j){
+  int temp=arr[i];
+  arr[i]=arr[j];
+  arr[j]=temp;
+}
+
+int main(){
+  int arr[] = {1,0,2,1,0,1,2,1,2};
+  dnfSort(arr,9);
+  
+  for(int i=0; i<9; i++){
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
+  
